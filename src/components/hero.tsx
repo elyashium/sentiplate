4d2d61f11c2782581a88d2e4b77b00580d53e9b1
Shash <ashishsinghlogic@gmail.com>
2024-11-15 01:07:05 +0530

    added title

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,9 +1,23 @@
 
 import "../App.css"
+import SearchBar from "./SearchBar";
 
 export const Hero = () => {
   return (
     <div className='hero'>
+
+
+      <div className="titleCard">
+        <SearchBar />
+        <h1>Sentiplate</h1>
+        <div className="titleDescription">
+          Find the perfect recipe for your every mood.
+        </div>
+        <div className="tryit">
+          <img src="src\images\tryitOut.svg" alt="" />
+        </div>
+
+      </div>
 
       <div className="topLeft">
         <img src="src\images\topLeft.png" alt="top left bowl" className="autoRotate" />
