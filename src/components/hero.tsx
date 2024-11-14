ba21aa80d7eb8a4b4816dde81a7badda79c9b161
Shash <ashishsinghlogic@gmail.com>
2024-11-14 23:18:32 +0530

    bowls added

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,7 +4,19 @@ import "../App.css"
 export const Hero = () => {
   return (
     <div className='hero'>
-     
+
+      <div className="topLeft">
+        <img src="src\images\topLeft.png" alt="top left bowl" />
+      </div>
+
+      <div className="redBowl">
+        <img src="src\images\Redbowl.png" alt="red bowl" />
+      </div>
+
+      <div className="whiteRightDown">
+        <img src="src\images\white right.png" alt="white bowl right down" />
+      </div>
+
     </div>
   )
 }
