a5f857b8076a8cf5e4fe1d86d73ec88d086cb2f2
Shash <ashishsinghlogic@gmail.com>
2024-11-15 10:34:36 +0530

    Update hero.tsx

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -8,47 +8,47 @@ export const Hero = () => {
   const navigate = useNavigate();
 
   const handleButtonClick = () => {
-    navigate("/MoodBasedRecommendation"); 
+    navigate("/MoodBasedRecommendation");
   };
 
   return (
     <main>
 
-    <div className='hero'>
+      <div className='hero'>
 
 
-      <div className="titleCard">
+        <div className="titleCard">
 
 
-        <h1 className="title">Sentiplate.</h1>
-        <div className="titleDescription">
-          Find the perfect recipe for your every mood.
+          <h1 className="title">Sentiplate.</h1>
+          <div className="titleDescription">
+            Find the perfect recipe for your every mood.
+          </div>
+
+          <div onClick={handleButtonClick} className="tryit">
+
+            <div className="btn">Try it out</div>
+            <img src="src\images\arrow.svg" alt="" />
+
+          </div>
+
         </div>
 
-        <div onClick={handleButtonClick} className="tryit">
-
-          <div className="btn">Try it out</div>
-          <img src="src\images\arrow.svg" alt="" />
-
+        <div className="topLeft">
+          <img src="src\images\topLeft.png" alt="top left bowl" className="autoRotate" />
         </div>
 
+        <div className="redBowl">
+          <img src="src\images\Redbowl.png" alt="red bowl" className="autoRotate" />
+        </div>
+
+        <div className="whiteRightDown">
+          <img src="src\images\whiteRightDown.png" alt="white bowl right " className="autoRotate" />
+        </div>
+
+
+
       </div>
-
-      <div className="topLeft">
-        <img src="src\images\topLeft.png" alt="top left bowl" className="autoRotate" />
-      </div>
-
-      <div className="redBowl">
-        <img src="src\images\Redbowl.png" alt="red bowl" className="autoRotate" />
-      </div>
-
-      <div className="whiteRightDown">
-        <img src="src\images\whiteRightDown.png" alt="white bowl right " className="autoRotate" />
-      </div>
-
-     
-
-    </div>
     </main>
   )
 }
