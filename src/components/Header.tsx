030150e936616511efd204620a089980a9eb048a
Shash <ashishsinghlogic@gmail.com>
2024-11-15 09:26:51 +0530

    used acternity

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+
+
+export default function Header() {
+
+
+
+
+  return (
+    <div className="head">
+        <div className="home"> Home </div>
+        <div className="about"> About Us </div>
+        <div className="recipe"> Recipes </div>
+        <div className="profile"> Profile </div>
+       
+    </div>
+  )
+}
--- src/components/SecondSection.tsx
+++ src/components/SecondSection.tsx
@@ -3,7 +3,6 @@
 export const SecondSection = () => {
     return (
         <div className="second-section">
-<h1>heelp</h1>
         </div>
     );
 };
