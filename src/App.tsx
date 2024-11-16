f3b4d597c466b5ad9610b9cc0e85effc5460b4b6
Shash <ashishsinghlogic@gmail.com>
2024-11-16 09:50:13 +0530

    app

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,7 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import MoodPage from "./components/MoodPage";
 import {Auth} from "./components/Auth";
-import LearnMore from "./components/LearnMore";
-import SuggestionBox from "./components/UserSuggestion";
+
 
 export default function App() {
   return (
@@ -13,8 +12,7 @@ export default function App() {
         <Route path="/home" element={<Hero />} />
         <Route path ="/" element ={<Auth />} />
         <Route path="/MoodBasedRecommendation" element={<MoodPage />} />
-        <Route path ="/LearnMore" element={<LearnMore/>} />
-        <Route path ="/UserSuggestion" element={<SuggestionBox/>} />
+        
       </Routes>
     </Router>
   )
